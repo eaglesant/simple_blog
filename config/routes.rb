6dd712b30fdc65c574bd05196745660a8feaa352
eaglesant <eagle_21@mail.ru>
2017-04-17 21:51:30 +0300

    HomeWork less_38

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   get 'home/index'
+  root 'home#index'
+  get '/contacts', to: 'contacts#new'
+  get '/articles', to: 'articles#new'
+  get '/about', to: 'about#show'
+  get '/terms', to: 'terms#show'
 
   resources :articles
   resource :contacts, only: [:new, :create], path_names: { :new => ''}
+  resource :about, only: [:show], path_names: { :show => ''}
+  resource :terms, only: [:show], path_names: { :show => ''}
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
